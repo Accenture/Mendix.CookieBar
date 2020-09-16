6cab8ac64c3f80ac3ab49ebe5578d74772e3b8f4
Kacper Kozimor <kkozimor@objectivity.co.uk>
2020-09-16 11:18:19 +0200

    slight update

--- src/CookieBar.jsx
+++ src/CookieBar.jsx
@@ -38,58 +38,59 @@ export default class CookieBar extends Component {
 
     render() {
         return (
-            <CookieConsent
-                //General
-                location={this.props.location}
-                overlay={this.props.overlay}
-                //Buttons
-                buttonId={this.fixEmptyString(this.props.buttonId)}
-                buttonText={this.fixEmptyString(this.props.buttonText)}
-                onAccept={this.props.onAccept !== undefined ? this.props.onAccept.execute : undefined}
-                declineButtonId={this.fixEmptyString(this.props.declineButtonId)}
-                declineButtonText={this.fixEmptyString(this.props.declineButtonText)}
-                onDecline={this.props.onDecline !== undefined ? this.props.onDecline.execute : undefined}
-                enableDeclineButton={this.props.enableDeclineButton}
-                flipButtons={this.props.flipButtons}
-                //Classes
-                style={Object.assign(
-                    this.props.style,
-                    this.state.message === "" ? { display: "none" } : { display: "" }
-                )}
-                containerClasses={"cookiebar-widget " + this.props.class}
-                buttonClasses="btn btn-primary btn-accept"
-                declineButtonClasses="btn btn-default btn-decline"
-                buttonWrapperClasses="btn-wrapper"
-                contentClasses="cookiebar-content"
-                overlayClasses="cookiebar-overlay"
-                disableStyles={true}
-                disableButtonStyles={true}
-                //Settings
-                hideOnAccept={this.props.hideOnAccept}
-                acceptOnScroll={this.props.acceptOnScroll}
-                acceptOnScrollPercentage={this.props.acceptOnScrollPercentage}
-                cookieName={this.fixEmptyString(this.props.cookieName)}
-                cookieValue={this.fixEmptyString(this.props.cookieValue)}
-                declineCookieValue={this.fixEmptyString(this.props.declineCookieValue)}
-                setDeclineCookie={this.props.setDeclineCookie}
-                debug={this.props.debug}
-                expires={this.props.expires}
-                sameSite={this.props.sameSite}
-                cookieSecurity={this.props.cookieSecurity}
-            >
-                <div id="cookiebar-wrapper" className="cookiebar-wrapper"></div>
-                {Parser(this.state.message)}
-                {this.props.useCookiesPolicy ? (
-                    <button
-                        className="btn-link mx-link btn-policy"
-                        onClick={this.props.onPolicyClick !== undefined ? this.props.onPolicyClick.execute : undefined}
-                    >
-                        {this.props.policyLinkText}
-                    </button>
-                ) : (
-                    ""
-                )}
-            </CookieConsent>
+            <div id="cookiebar-id" className={"cookiebar " + this.props.class} style={this.props.style}>
+                <CookieConsent
+                    //General
+                    location={this.props.location}
+                    overlay={this.props.overlay}
+                    //Buttons
+                    buttonId={this.fixEmptyString(this.props.buttonId)}
+                    buttonText={this.fixEmptyString(this.props.buttonText)}
+                    onAccept={this.props.onAccept !== undefined ? this.props.onAccept.execute : undefined}
+                    declineButtonId={this.fixEmptyString(this.props.declineButtonId)}
+                    declineButtonText={this.fixEmptyString(this.props.declineButtonText)}
+                    onDecline={this.props.onDecline !== undefined ? this.props.onDecline.execute : undefined}
+                    enableDeclineButton={this.props.enableDeclineButton}
+                    flipButtons={this.props.flipButtons}
+                    //Classes
+                    style={this.state.message === "" ? { display: "none" } : { display: "" }}
+                    containerClasses={"cookiebar-widget "}
+                    buttonClasses="btn btn-primary btn-accept"
+                    declineButtonClasses="btn btn-default btn-decline"
+                    buttonWrapperClasses="btn-wrapper"
+                    contentClasses="cookiebar-content"
+                    overlayClasses="cookiebar-overlay"
+                    disableStyles={true}
+                    disableButtonStyles={true}
+                    //Settings
+                    hideOnAccept={this.props.hideOnAccept}
+                    acceptOnScroll={this.props.acceptOnScroll}
+                    acceptOnScrollPercentage={this.props.acceptOnScrollPercentage}
+                    cookieName={this.fixEmptyString(this.props.cookieName)}
+                    cookieValue={this.fixEmptyString(this.props.cookieValue)}
+                    declineCookieValue={this.fixEmptyString(this.props.declineCookieValue)}
+                    setDeclineCookie={this.props.setDeclineCookie}
+                    debug={this.props.debug}
+                    expires={this.props.expires}
+                    sameSite={this.props.sameSite}
+                    cookieSecurity={this.props.cookieSecurity}
+                >
+                    <div id="cookiebar-wrapper" className="cookiebar-wrapper"></div>
+                    {Parser(this.state.message)}
+                    {this.props.useCookiesPolicy ? (
+                        <button
+                            className="btn-link mx-link btn-policy"
+                            onClick={
+                                this.props.onPolicyClick !== undefined ? this.props.onPolicyClick.execute : undefined
+                            }
+                        >
+                            {this.props.policyLinkText}
+                        </button>
+                    ) : (
+                        ""
+                    )}
+                </CookieConsent>
+            </div>
         );
     }
 }
